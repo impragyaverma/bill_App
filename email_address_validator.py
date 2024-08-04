15aa47df1e7dc415b5d9d8273ab5412c6d572565
iampragyaverma <pragya9428@gmail.com>
2024-08-04 17:07:39 +0530

    first commit

---
+++ email_address_validator.py
@@ -0,0 +1,27 @@
+# Python program to validate an Email
+ 
+# import re module
+ 
+# re module provides support
+# for regular expressions
+import re
+ 
+# Make a regular expression
+# for validating an Email
+regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+ 
+# Define a function for
+# for validating an Email
+ 
+ 
+def check(email):
+ 
+    # pass the regular expression
+    # and the string in search() method
+    if(re.match(regex, email)):
+        print("Valid Email")
+        return True
+ 
+    else:
+        print("Invalid Email")
+        return False
